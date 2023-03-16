7718a9a7734d6ab43f7563655b1b65be5fb4bd18
cele-perei <celesteavei@gmail.com>
2023-03-16 19:30:24 -0300

    first commit

---
+++ public/js/socket-clients.js
@@ -0,0 +1,39 @@
+//Referencias HTML
+const lblOnline = document.querySelector('#lblOnline');
+const lblOffline = document.querySelector('#lblOffline');
+const txtMensaje = document.querySelector('#txtMensaje');
+const btnEnviar = document.querySelector('#btnEnviar');
+
+const socket = io();
+
+socket.on('connect', () => {
+
+    //console.log('Conectado');
+    lblOffline.style.display = 'none';
+    lblOnline.style.display = '';
+
+});
+
+socket.on('disconnect', () => {
+    lblOffline.style.display = '';
+    lblOnline.style.display = 'none';
+    //console.log('Desconectado');
+
+});
+
+socket.on('enviar-mensaje', (payload) => {
+    console.log(payload);
+});
+
+btnEnviar.addEventListener('click', () => {
+    const mensaje = txtMensaje.value;
+    const payload = {
+        mensaje,
+        id: '6222557',
+        fecha: new Date().getTime()
+    }
+    socket.emit('enviar-mensaje', payload, (id) => {
+        console.log('Desde el server, ', id);
+    });
+
+});
